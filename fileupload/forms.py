3f54bb2b648e0a0d4f2a981d2ff7413864785277
lulu <129.911@gmail.com>
2012-12-07 13:10:34 +0800

    encoding bug

--- fileupload/forms.py
+++ fileupload/forms.py
@@ -14,7 +14,7 @@ class RequirementForm(ModelForm):
     copies = forms.IntegerField(error_messages = {'required': u'忘记写份数了哦','max_value':u'最多50份啦','min_value':u'至少1份诶'},min_value=1, max_value = 50)
     def clean(self):
         note = self.cleaned_data['note']
-        print note
+        # print note
         range = self.cleaned_data['range']
         #print range
         if (range == u"自定义")and(note == " "):
